2692ce7311e8c29379589a937514ec8c31c5d9c8
luisprog1 <luis.alberto.viana.dantas@ccc.ufcg.edu.br>
2025-07-26 17:33:04 -0300

    Criando instâncias de salas e turmas

--- haskell/src/Main.hs
+++ haskell/src/Main.hs
@@ -1,5 +1,22 @@
 module Main (main) where
 
+import Tipos
+import Alocador
+
+classroom :: Classroom
+class1 :: Class
+classroom = Classroom { classroomId = 1, capacity = 30, block = "Block A", resources = [Projector, Laboratory]}
+class1 = Class { classId = 1, subject = "Physics", course ="Engineering", professor = "Dr. Smith", schedule = "Mon 10-12", size = 25, requirements = [Projector, Laboratory]}
+
+getclassroomId :: Classroom -> String
+getclassroomId (Classroom {block = bloco}) = bloco
+
+getclassSubject :: Class -> String
+getclassSubject (Class {subject = materia}) = materia
+
 main :: IO ()
 main = do
-  putStrLn "hello world"
+    putStrLn "Alocador de Turmas e Salas"
+    putStrLn $ "Id da turma " ++ getclassroomId classroom
+    putStrLn $ "Matéria da turma: " ++ getclassSubject class1
+--    let class2 = Class { classId = 2, subject = "Chemistry", course = "Science", professor = "Dr. Jones", schedule = "Tue 14-16", size = 35, requirements = [Acessibility] }
--- haskell/src/Tipos.hs
+++ haskell/src/Tipos.hs
@@ -2,32 +2,28 @@ module Tipos where
 
 import GHC.Generics (Generic)
 import Data.Aeson (ToJSON, FromJSON)
--- Tipos de ID para clareza e segurança de tipo
-newtype ClassroomId = SalaId Int deriving (Show, Eq, Ord, Generic, ToJSON, FromJSON)
-newtype ClassId = TurmaId Int deriving (Show, Eq, Ord, Generic, ToJSON, FromJSON)
-
 
 data Resource = Projector | Laboratory | Acessibility | Other String
 
 data Class = Class {  
-classId :: ClassId,
+classId :: Int,
 subject :: String,
 course :: String,
 professor :: String,
 schedule :: String,
 size :: Int,
 requirements :: [Resource]
-} deriving ()
+}
 
 data Classroom = Classroom { 
-classroomId :: ClassroomId,
+classroomId :: Int,
 capacity :: Int,
 block :: String,
 resources :: [Resource]
-} deriving ()
+}
 
 data Allocation = Allocation { 
 allocationId :: Int, 
-allocClassId :: ClassId,
-allocClassroomId :: ClassroomId
+allocClassId :: Int,
+allocClassroomId :: Int
 }
