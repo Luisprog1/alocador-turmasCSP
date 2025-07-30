edf497dd7fb89f2ddc31f8dddd7ea840cfa41644
Eduardo <eduardosume082@gmail.com>
2025-07-29 23:33:58 -0300

    feat: added register screen

--- haskell/src/repository/UserRepository.hs
+++ haskell/src/repository/UserRepository.hs
@@ -1,9 +1,19 @@
 module Repository.UserRepository where
 
-register_User :: Int -> String -> IO ()
-register_User matricula pass = do
-    putStrLn $ "matricula: " ++ show matricula ++ " password: " ++ pass
+saveUser :: Int -> String -> IO ()
+saveUser matricula pass = do
     writeFile "src/data/user.txt" (show matricula ++ " " ++ pass)
+
+register_User :: Int -> String -> String -> IO ()
+register_User matricula pass senhaConf = do
+    if pass == senhaConf
+        then do
+            saveUser matricula pass
+            putStrLn "User registered successfully!"
+        else
+            putStrLn "Passwords do not match!"
+
+
 login_User :: Int -> String -> IO ()
 login_User matricula pass = do
     content <- readFile "src/data/user.txt"
