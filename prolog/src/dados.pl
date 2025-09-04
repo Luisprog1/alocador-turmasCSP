dbb19c547e20130adb4dc02725ec4f43cb9f518a
jennifermedeiross <jenniferraysa12.jr@gmail.com>
2025-09-04 09:46:09 -0300

    feat: add fatos das disciplinas

--- prolog/src/dados.pl
+++ prolog/src/dados.pl
@@ -1,8 +1,91 @@
-disciplina('Banco de Dados').
-disciplina('Inteligência Artificial').
-disciplina('Programação Web').
-disciplina('Redes de Computadores').
-disciplina('Sistemas Operacionais').
+disciplina("Álgebra Linear I").
+disciplina("Análise de Sistemas").
+disciplina("Análise e Técnicas de Algoritmos").
+disciplina("Banco de Dados I").
+disciplina("Cálculo Diferencial e Integral I").
+disciplina("Cálculo Diferencial e Integral II").
+disciplina("Compiladores").
+disciplina("Direito e Cidadania").
+disciplina("Economia").
+disciplina("Engenharia de Software").
+disciplina("Estatística Aplicada").
+disciplina("Estruturas de Dados e Algoritmos").
+disciplina("Fundamentos de Matemática para Ciência da Computação I").
+disciplina("Fundamentos de Matemática para Ciência da Computação II").
+disciplina("Inteligência Artificial").
+disciplina("Introdução à Computação").
+disciplina("Introdução à Probabilidade").
+disciplina("Laboratório de Estruturas de Dados e Algoritmos").
+disciplina("Laboratório de Programação I").
+disciplina("Laboratório de Programação II").
+disciplina("Língua Portuguesa").
+disciplina("Lógica para Computação").
+disciplina("Metodologia Científica").
+disciplina("Organização e Arquitetura de Computadores").
+disciplina("Paradigmas de Linguagens de Programação").
+disciplina("Programação Concorrente").
+disciplina("Programação I").
+disciplina("Programação II").
+disciplina("Projeto de Software").
+disciplina("Projeto em Computação I").
+disciplina("Projeto em Computação II").
+disciplina("Redes de Computadores").
+disciplina("Sistemas Operacionais").
+disciplina("Teoria da Computação").
+disciplina("Teoria dos Grafos").
+disciplina("Trabalho de Conclusão de Curso").
+disciplina("Administração de Sistemas").
+disciplina("Administração de Sistemas Gerenciadores de Bancos de Dados").
+disciplina("Algoritmos Avançados I").
+disciplina("Algoritmos Avançados II").
+disciplina("Algoritmos Avançados III").
+disciplina("Algoritmos Avançados IV").
+disciplina("Arquitetura de Software").
+disciplina("Avaliação de Desempenho de Sistemas Discretos").
+disciplina("Banco de Dados II").
+disciplina("Ciência de Dados Descritiva").
+disciplina("Ciência de Dados Preditiva").
+disciplina("Computação e Música").
+disciplina("Computação Gráfica").
+disciplina("Desenvolvimento de Software Integrado à Operação da Infraestrutura").
+disciplina("Economia de Tecnologia da Informação").
+disciplina("Empreendedorismo e Inovação").
+disciplina("Gerência de Redes").
+disciplina("Gestão de Projetos").
+disciplina("Governança da Internet").
+disciplina("Habilidades Socioemocionais I").
+disciplina("Habilidades Socioemocionais II").
+disciplina("Informática e Sociedade").
+disciplina("Inglês").
+disciplina("Interconexão de Redes de Computadores").
+disciplina("Interface Humano-Computador").
+disciplina("Jogos Digitais").
+disciplina("Libras").
+disciplina("Métodos e Software Numéricos").
+disciplina("Métodos Formais").
+disciplina("Otimização").
+disciplina("Percepção Computacional").
+disciplina("Prática de Ensino em Computação I").
+disciplina("Prática de Ensino em Computação II").
+disciplina("Processamento de Linguagem Natural").
+disciplina("Programação em Banco de Dados").
+disciplina("Programação Funcional").
+disciplina("Programação para a Web I").
+disciplina("Programação para a Web II").
+disciplina("Projeto de Redes de Computadores").
+disciplina("Projeto de Sistemas Digitais").
+disciplina("Provisionamento e Operação de Infraestrutura").
+disciplina("Reconhecimento de Padrões e Redes Neurais").
+disciplina("Recuperação da Informação e Busca na Web").
+disciplina("Segurança de Sistemas").
+disciplina("Sistemas de Apoio à Decisão").
+disciplina("Sistemas de Informações Geográficas").
+disciplina("Sistemas Distribuídos").
+disciplina("Tópicos em Ciência da Computação I").
+disciplina("Tópicos em Ciência da Computação II").
+disciplina("Transformação Digital").
+disciplina("Verificação e Validação de Software").
+disciplina("Visualização de Dados").
 curso('Ciência da Computação').
 curso('Engenharia de Software').
 curso('Sistemas de Informação').
