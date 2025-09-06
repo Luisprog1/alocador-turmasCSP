7af84fd4558aa4bbad0cc5d9243d550eef9a3749
claraugusta <mc.augusta1052@gmail.com>
2025-09-06 12:54:27 -0300

    acentuacoes

--- prolog/src/schedule.pl
+++ prolog/src/schedule.pl
@@ -5,7 +5,7 @@
 :- dynamic(horario_ocupado/4).
 
 menu_dia("1", 'Segunda') :- !.
-menu_dia("2", 'Terca'):- !.
+menu_dia("2", 'Terça'):- !.
 menu_dia("3", 'Quarta'):- !.
 menu_dia("4", 'Quinta'):- !.
 menu_dia("5", 'Sexta'):- !.
@@ -19,28 +19,28 @@ menu_hora("6", '20-22'):- !.
 
 read_day(Id_turma, Id_sala, Dia, Hora):-
     write('Escolha um dia:\n'),
-    write('1. Segunda   2. Terca    3. Quarta\n4. Quinta    5. Sexta\n'),
+    write('1. Segunda   2. Terça    3. Quarta   4. Quinta    5. Sexta\n'),
     read_line_to_string(user_input, Dia_op),
     ( menu_dia(Dia_op, Dia) ->
         true
     ;
-        write('\nOpcao invalida, tente novamente.\n'), nl,
+        write('\nOpção inválida, tente novamente.\n'), nl,
         read_day(Id_turma, Id_sala, New_Dia, New_Hora)  
     ),
     write('Escolha o horario:\n'),
-    write('1. [08-10]   2. [10-12]  3. [14-16]\n4. [16-18]  5. [18-20]  6.[20-22]\n'),
+    write('\n1. [08-10]   2. [10-12]  3. [14-16]\n4. [16-18]  5. [18-20]  6.[20-22]\n'),
     read_line_to_string(user_input, Hora_op),
     ( menu_hora(Hora_op, Hora) ->
         true
     ;
-        write('\nOpcao invalida, tente novamente.\n'), nl,
+        write('\nOpção inválida, tente novamente.\n'), nl,
         read_day(Id_turma, Id_sala, New_Dia, New_Hora) 
     ),
-    (   horario_ocupado(_, Id_sala, Dia, Hora) -> write('\nErro: sala ja ocupada nesse dia e horario.\n'), nl, read_day(Id_turma, Id_sala, New_Dia, New_Hora);
-        horario_ocupado(Id_turma, _, Dia, Hora) -> write('\nErro: turma ja possui aula nesse dia e horario.\n'), nl, read_day(Id_turma, Id_sala, New_Dia, New_Hora);
+    (   horario_ocupado(_, Id_sala, Dia, Hora) -> write('\nErro: sala já ocupada nesse dia e horário.\n'), nl, read_day(Id_turma, Id_sala, New_Dia, New_Hora);
+        horario_ocupado(Id_turma, _, Dia, Hora) -> write('\nErro: turma já possui aula nesse dia e horário.\n'), nl, read_day(Id_turma, Id_sala, New_Dia, New_Hora);
         assertz(horario_ocupado(Id_turma, Id_sala, Dia, Hora)),
         save_ocupacao_sala('rules/horarios_ocupados_sala.pl'),
-        write('Horario adicionado com sucesso.'), nl,!
+        write('Horário adicionado com sucesso.'), nl,!
     ), 
     write('Deseja adicionar mais algum horário? (s/n) '), read_line_to_string(user_input, Choice),
             Choice = "s" ->  read_day(Id_turma, Id_sala, New_Dia, New_Hora).
