6b5dd6917b4928745101d4133deb7ca4c65efae1
luisprog1 <luis.alberto.viana.dantas@ccc.ufcg.edu.br>
2025-09-10 19:52:31 -0300

    modified:   main.pl 	modified:   rules/classes.pl 	modified:   rules/horarios_turmas.pl

---
+++ prolog/src/rules/classes.pl
@@ -0,0 +1,2 @@
+class("4","Teoria dos Grafos","Ciência da Computação","Luis",35,["Quadro Branco","Projetor"]).
+class("5","Programação II","Ciência da Computação","Clara",48,["Quadro Branco","Projetor"]).
