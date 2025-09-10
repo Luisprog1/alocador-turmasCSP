ce6c090077f16908dacb73278bf46569fbde7584
jennifermedeiross <jenniferraysa12.jr@gmail.com>
2025-09-09 23:21:18 -0300

    feta: gerador de interface como em haskell

--- prolog/src/interfaces/userInterface.pl
+++ prolog/src/interfaces/userInterface.pl
@@ -1,11 +1,9 @@
 :- encoding(utf8).
 :- ensure_loaded('repository/userRepository.pl').
+:- ensure_loaded('interfaces/UI.pl').
 
 user_screen :-
-    nl,
-    write('============================'), nl,
-    write('     BEM-VINDO AO SISTEMA   '), nl,
-    write('============================'), nl,
+    draw_header('BEM-VINDO AO SISTEMA'),
     write('1 - Registrar'), nl,
     write('2 - Login'), nl,
     write('============================'), nl,
@@ -17,9 +15,9 @@ user_screen :-
     ).
 
 register_screen :-
-    nl, write('--- CADASTRO DE USUÁRIO ---'), nl,
+    draw_header('CADASTRO DE USUÁRIO'),
     entry_user.
 
 login_screen :-
-    nl, write('--- LOGIN ---'), nl,
+    draw_header('LOGIN'),
     ( login_user -> true ; user_screen ).
