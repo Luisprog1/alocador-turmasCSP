23d6d86c0ecbfe131c2f275c8e9f9d1ec3db282a
claraugusta <mc.augusta1052@gmail.com>
2025-09-11 13:38:33 -0300

    feat: view do professor

---
+++ prolog/src/interfaces/profInterface.pl
@@ -0,0 +1,29 @@
+:- use_module(library(strings)).
+:- ensure_loaded('../dados.pl').
+:- ensure_loaded('../repository/save.pl').
+:- ensure_loaded('../repository/utilsClass.pl').
+:- ensure_loaded('../validacao.pl').
+:- ensure_loaded('../main.pl').
+:- ensure_loaded('userInterface.pl').
+:- ensure_loaded('UI.pl').
+:- encoding(utf8).
+
+professor_menu :-
+    draw_header("PROFESSOR"),
+    write('1. Visualizar turmas'), nl,
+    write('2. Alterar requisitos da turma'), nl,
+    write('3. Sair e salvar'), nl,
+    write('Opção: '),
+    read_line_to_string(user_input, Opcao),
+    professor_op(Opcao).
+
+% Tratamento das opções
+professor_op("1") :-
+    write('Visualizar turmas'), nl.
+professor_op("2") :-
+    write('Alterar requisitos da turma'), nl.
+professor_op("3") :-
+    write('Saindo e salvando...'), nl, user_screen.
+professor_op(_) :-
+    write('Opção inválida! Tente novamente.'), nl,
+    professor_menu.
