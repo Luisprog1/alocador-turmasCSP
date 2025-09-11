496cbd29c746d6690ba6fba7992ff232ed596658
luisprog1 <luis.alberto.viana.dantas@ccc.ufcg.edu.br>
2025-09-11 11:42:49 -0300

    feat: adicionar função de editar recurso de salas

--- prolog/src/rules/classrooms.pl
+++ prolog/src/rules/classrooms.pl
@@ -1,3 +1,3 @@
-classroom("2","CP",30,"Projetor").
-classroom("3","CD",50,"Projetor").
-classroom("5","CP",120,"Projetor").
+classroom("5","CP",120,["Projetor"]).
+classroom("2","CP",30,["Projetor"]).
+classroom("3","CD",50,["Laboratório","Quadro Branco","Acessibilidade","Projetor"]).
