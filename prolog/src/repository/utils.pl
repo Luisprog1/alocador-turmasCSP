c7e6739fb18918b7f89474bf1facd6b954c5d87d
jennifermedeiross <jenniferraysa12.jr@gmail.com>
2025-09-11 21:44:48 -0300

    feat: utilitario para pausar interface e print de sucesso e erro

---
+++ prolog/src/repository/utils.pl
@@ -0,0 +1,12 @@
+:- encoding(utf8).
+:- use_module(library(ansi_term)).
+
+pause :-
+    ansi_format([fg(yellow)], "~w", ["Pressione ENTER para continuar..."]),
+    read_line_to_string(user_input, _).
+
+print_erro(Msg) :-
+    ansi_format([fg(red)], "~w~n", [Msg]).
+
+print_sucesso(Msg) :-
+    ansi_format([fg(green)], "~w~n", [Msg]).
