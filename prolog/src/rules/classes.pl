098b2be3dee62b9d8464bb3d65900fa1f558fe5c
Maria Clara <136365112+claraugusta@users.noreply.github.com>
2025-09-14 18:36:48 -0300

    Update classes.pl

--- prolog/src/rules/classes.pl
+++ prolog/src/rules/classes.pl
@@ -1,4 +1,36 @@
-class("5","Programação II","Ciência da Computação","1",48,["Quadro Branco","Projetor"]).
-class("1","Programação I","Ciência da Computação","1",48,["Quadro Branco"]).
-class("2","Projeto de Software","Ciência da Computação","1",48,["Quadro Branco","Projetor", "Acessibilidade", "Laboratório"]).
-class("4","Teoria dos Grafos","Ciência da Computação","0",35,["Acessibilidade"]).
+class("101", "Álgebra Linear I", "Ciência da Computação", "2025001", 50, ["Projetor", "Quadro Branco"]).
+class("102", "Análise de Sistemas", "Sistemas de Informação", "2025002", 45, ["Projetor", "Quadro Branco", "Acessibilidade"]).
+class("103", "Análise e Técnicas de Algoritmos", "Ciência da Computação", "2025003", 40, ["Projetor", "Quadro Branco"]).
+class("104", "Banco de Dados I", "Sistemas de Informação", "2025004", 35, ["Projetor", "Laboratório"]).
+class("105", "Cálculo Diferencial e Integral I", "Ciência da Computação", "2025005", 60, ["Quadro Branco"]).
+class("106", "Cálculo Diferencial e Integral II", "Ciência da Computação", "2025005", 55, ["Quadro Branco"]).
+class("107", "Compiladores", "Ciência da Computação", "2025006", 30, ["Projetor", "Quadro Branco"]).
+class("108", "Direito e Cidadania", "Engenharia de Software", "2025007", 50, ["Projetor"]).
+class("109", "Economia", "Sistemas de Informação", "2025008", 50, ["Projetor", "Quadro Branco"]).
+class("110", "Engenharia de Software", "Engenharia de Software", "2025009", 40, ["Projetor", "Acessibilidade", "Quadro Branco"]).
+class("111", "Estatística Aplicada", "Ciência da Computação", "2025010", 45, ["Projetor", "Quadro Branco"]).
+class("112", "Estruturas de Dados e Algoritmos", "Ciência da Computação", "2025011", 40, ["Projetor", "Quadro Branco", "Laboratório"]).
+class("113", "Fundamentos de Matemática para Ciência da Computação I", "Ciência da Computação", "2025012", 60, ["Quadro Branco"]).
+class("114", "Fundamentos de Matemática para Ciência da Computação II", "Ciência da Computação", "2025012", 55, ["Quadro Branco"]).
+class("115", "Inteligência Artificial", "Ciência da Computação", "2025013", 35, ["Projetor", "Laboratório"]).
+class("116", "Introdução à Computação", "Análise e Desenvolvimento de Sistemas", "2025014", 60, ["Projetor", "Quadro Branco"]).
+class("117", "Introdução à Probabilidade", "Ciência da Computação", "2025010", 45, ["Projetor", "Quadro Branco"]).
+class("118", "Laboratório de Estruturas de Dados e Algoritmos", "Ciência da Computação", "2025011", 30, ["Laboratório"]).
+class("119", "Laboratório de Programação I", "Análise e Desenvolvimento de Sistemas", "2025015", 30, ["Laboratório"]).
+class("120", "Laboratório de Programação II", "Análise e Desenvolvimento de Sistemas", "2025015", 30, ["Laboratório"]).
+class("121", "Língua Portuguesa", "Sistemas de Informação", "2025016", 50, ["Projetor", "Quadro Branco"]).
+class("122", "Lógica para Computação", "Ciência da Computação", "2025017", 40, ["Projetor", "Quadro Branco"]).
+class("123", "Metodologia Científica", "Engenharia de Software", "2025018", 50, ["Projetor"]).
+class("124", "Organização e Arquitetura de Computadores", "Ciência da Computação", "2025019", 40, ["Projetor", "Quadro Branco", "Laboratório"]).
+class("125", "Paradigmas de Linguagens de Programação", "Ciência da Computação", "2025020", 35, ["Projetor", "Quadro Branco"]).
+class("126", "Programação Concorrente", "Ciência da Computação", "2025021", 30, ["Projetor", "Laboratório"]).
+class("127", "Programação I", "Análise e Desenvolvimento de Sistemas", "2025022", 50, ["Projetor", "Quadro Branco", "Laboratório"]).
+class("128", "Programação II", "Análise e Desenvolvimento de Sistemas", "2025022", 45, ["Projetor", "Quadro Branco", "Laboratório"]).
+class("129", "Projeto de Software", "Engenharia de Software", "2025009", 40, ["Projetor", "Quadro Branco"]).
+class("130", "Projeto em Computação I", "Ciência da Computação", "2025023", 25, ["Projetor", "Acessibilidade"]).
+class("131", "Projeto em Computação II", "Ciência da Computação", "2025023", 25, ["Projetor", "Acessibilidade"]).
+class("132", "Redes de Computadores", "Sistemas de Informação", "2025024", 40, ["Projetor", "Laboratório"]).
+class("133", "Sistemas Operacionais", "Ciência da Computação", "2025025", 40, ["Projetor", "Quadro Branco", "Laboratório"]).
+class("134", "Teoria da Computação", "Ciência da Computação", "2025026", 35, ["Projetor", "Quadro Branco"]).
+class("135", "Teoria dos Grafos", "Ciência da Computação", "2025027", 30, ["Projetor", "Quadro Branco"]).
+class("136", "Trabalho de Conclusão de Curso", "Engenharia de Software", "2025028", 20, []).
