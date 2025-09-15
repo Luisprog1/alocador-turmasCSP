037a2336652406518b4cf49c1c8f86e0ab161cc4
jennifermedeiross <jenniferraysa12.jr@gmail.com>
2025-09-14 21:47:23 -0300

    chore: ajusta erros de validacao e melhora fluxo

--- prolog/src/validacao.pl
+++ prolog/src/validacao.pl
@@ -6,37 +6,40 @@
 read_classId(ID) :-
     write('ID da turma: '), read_line_to_string(user_input, Input),
     (number_string(Number, Input), Number > 0 ->
-        (class(Input, _, _, _, _, _) -> (write('Já existe uma turma com esse ID. Tente novamente.\n'), read_classId(ID)) 
+        (class(Input, _, _, _, _, _) -> (print_erro('Já existe uma turma com esse ID. Tente novamente.\n'), read_classId(ID)) 
         ; ID = Input)
     ).
 
 read_disciplina(id, Disciplina) :-
-    write('Informe o o número da disciplina:\n'),
+    draw_header("CADASTRO DE TURMA"),
+    write('Informe o número da disciplina:\n'),
     listar_disciplinas,
     read_line_to_string(user_input, Input),
     (disciplina(Input, Disciplina) -> true
-        ;write('Disciplina inválida. Tente novamente.\n'), read_disciplina(id, Disciplina)
+        ;print_erro('Disciplina inválida. Tente novamente.\n'), read_disciplina(id, Disciplina)
     ).
 
 read_curso(id,Curso) :-
+    draw_header("CADASTRO DE TURMA"),
     write('Informe o número do Curso:\n'),
     listar_cursos,
     read_line_to_string(user_input, Input),
     (curso(Input, Curso) -> true
-        ;write('Curso invalido. Tente novamente.\n'), read_curso(id,Curso)
+        ;print_erro('Curso invalido. Tente novamente.\n'), pause, read_curso(id,Curso)
     ).
 
 read_recursos(Acumulados, Requisitos) :-
     write('Escolha os Requisitos\n'),
-    write("1.Projetor   2.Acessibilidade    3.Quadro Branco     4.Laboratório\n"),
+    print_colorido("1.Projetor   2.Acessibilidade    3.Quadro Branco     4.Laboratório\n", green),
     read_line_to_string(user_input, Input),
     (recursos(Input, NomeRequisito) ->
         (member(NomeRequisito, Acumulados) ->  
-            write('Requisito já escolhido. Tente outro.\n'), read_recursos(Acumulados, Requisitos)
-            ;write('Deseja adicionar mais algum requisito? (s/n) '), read_line_to_string(user_input, Choice),
+            print_erro('Requisito já escolhido. Tente outro.\n'), read_recursos(Acumulados, Requisitos)
+            ;write('Deseja adicionar mais algum requisito? (s/n) '), read_line_to_string(user_input, ChoiceRaw),
+            string_lower(ChoiceRaw, Choice),
             (Choice = "s" ->  read_recursos([NomeRequisito|Acumulados], Requisitos) ; Requisitos = [NomeRequisito|Acumulados])
         )
-        ;write('Requisito inválido. Tente novamente.\n'), read_recursos(Acumulados, Requisitos)
+        ;print_erro('Requisito inválido. Tente novamente.\n'), read_recursos(Acumulados, Requisitos)
     ).
 
 read_classroomId(ID) :-
@@ -50,45 +53,51 @@ read_user_id(ID) :-
     write('ID do usuário: '), read_line_to_string(user_input, Input),
     (number_string(Number, Input), Number > 0 ->
         (user(Input, _, _, _) -> 
-        (write('Já existe um usuário com esse ID. Tente novamente.\n'), read_user_id(ID)) 
+        (print_erro('Já existe um usuário com esse ID. Tente novamente.\n'), pause, register_screen) 
         ; ID = Input)
-    ; write('ID inválido! Digite novamente.\n'),
+    ; print_erro('ID inválido! Digite novamente.\n'),
       read_user_id(ID)
     ).
 
 read_func_user(Role) :-
-    write('Função do usuário [0 | 1]:'), nl,
-    write('[0] Administrador'), nl,
-    write('[1] Professor'), nl,
+    print_colorido('Função do usuário:', yellow), nl,
+    print_colorido('[0] ', yellow),
+    write('Administrador'), nl,
+    print_colorido('[1] ', yellow),
+    write('Professor'), nl,
     write('Escolha: '),
     read_line_to_string(user_input, Input),
     ( Input = "0" ->
         ( user(_, _, _, "Admin") ->
-            write('Já existe um administrador cadastrado. Tente novamente.\n'),
-            read_func_user(Role)
+            print_erro('Já existe um administrador cadastrado. Tente novamente.\n'),
+            pause,
+            register_screen
         ; Role = "Admin"
         )
     ; Input = "1" ->
         Role = "Prof"
-    ; write('Opção inválida. Tente novamente.\n'),
+    ; print_erro('Opção inválida. Tente novamente.\n'),
       read_func_user(Role)
     ).
 
 read_capacity(Capacidade) :-
+    draw_header("CADASTRO DE TURMA"),
     write('Capacidade: '), 
     read_line_to_string(user_input, Input),
     ( number_string(Number, Input), Number > 0 ->
         Capacidade = Number
-    ; write('Capacidade inválida. Digite um número positivo.\n'),
+    ; print_erro('Capacidade inválida. Digite um número positivo.\n'),
       read_capacity(Capacidade)
     ).
 
 read_professor_id(ID) :-
+    draw_header("CADASTRO DE TURMA"),
+    listar_professores,
     write('Digite o ID do professor responsável: '),
     read_line_to_string(user_input, Input),
     ( user(Input, _, _, "Prof") ->
         ID = Input
-    ; write('Professor não encontrado! Digite apenas uma ID válida.\n'),
+    ; print_erro('Professor não encontrado! Digite apenas uma ID válida.\n'), pause,
       read_professor_id(ID)
     ).
 
@@ -97,14 +106,14 @@ valida_turma(ID_Turma) :-
     !.                             
 valida_turma(ID_Turma) :- 
     \+ class(ID_Turma, _, _, _, _, _), 
-    write("Erro: turma com ID "), write(ID_Turma), write(" não encontrada.\n"),
+    print_erro("Erro: turma com ID "), print_erro(ID_Turma), print_erro(" não encontrada.\n"),
     fail.                         
 
 get_classroom(ID) :-
     write('ID da sala: '), read_line_to_string(user_input, Input),
     (number_string(Number, Input), Number > 0 ->
         (classroom(Input, _, _, _) -> (ID = Input) 
-        ; (write('Não existe uma sala com esse ID. Tente novamente.\n'), get_classroom(ID)))
+        ; (print_erro('Não existe uma sala com esse ID. Tente novamente.\n'), get_classroom(ID)))
     ).
 
 listar_disciplinas :-
