b89a2a1a12e5c0372a1aee470ae34684e9c4f852
jennifermedeiross <jenniferraysa12.jr@gmail.com>
2025-09-14 21:48:48 -0300

    chore: ajusta mensagens de erro e traz funções que estavam no main

--- prolog/src/interfaces/adminInterface.pl
+++ prolog/src/interfaces/adminInterface.pl
@@ -7,10 +7,11 @@
 :- ensure_loaded('../schedule.pl').
 :- ensure_loaded('../main.pl').
 :- ensure_loaded('userInterface.pl').
+:- ensure_loaded('UI.pl').
 :- encoding(utf8).
 
 admin_menu :-
-    nl, write('ADMINISTRADOR'), nl,
+    draw_header("ADMINISTRADOR"),
     write('1. Gerar alocação'), nl,
     write('2. Visualizar Alocações'), nl,
     write('3. Cadastrar Sala'), nl,
@@ -25,17 +26,17 @@ admin_menu :-
 processar_opcao("1") :- write('Gerando alocacao...'), nl, admin_menu.
 processar_opcao("2") :- 
     write('Visualizando alocações...'), nl,
-    write('Pressione enter para sair.'),
-    read_line_to_string(user_input, n), 
+    pause,
     admin_menu.
 processar_opcao("3") :- entry_classroom, nl, admin_menu.
 processar_opcao("4") :- entry_class, nl, admin_menu.
 processar_opcao("5") :- submenu_sala, nl, admin_menu.
 processar_opcao("6") :- submenu_turma, nl, admin_menu.
 processar_opcao("7") :- write('Voltando a tela inicial...'), nl, user_screen.
-processar_opcao(_) :- write('Opcao invalida!'), nl, admin_menu.
+processar_opcao(_) :- print_erro('Opcao invalida!\n'), pause, admin_menu.
 
 submenu_sala :-
+    draw_header('MENU SALA'), 
     write('1 - Editar os recursos da sala'), nl,
     write('2 - Editar a capacidade da sala'), nl,
     write('3 - Voltar ao menu anterior'), nl,
@@ -48,29 +49,30 @@ submenu_sala :-
     ( classroom(ID, _, _, _) ->
         read_recursos([], Recursos),
         altera_recursos_classroom(ID, Recursos),
-        write('Recursos atualizados com sucesso!'), nl
-    ; write('Sala não encontrada!'), nl
+        print_sucesso('Recursos atualizados com sucesso!'), nl
+    ; print_erro('Sala não encontrada!'), nl
     ),
-    nl, submenu_sala.
+    pause, submenu_sala.
 
 processar_submenu_sala("2") :-
     write('ID da sala: '), read_line_to_string(user_input, ID),
     ( classroom(ID, _, _, _) ->
         read_capacity(Capacidade),
         update_capacity(ID, Capacidade),
-        write('Capacidade atualizada com sucesso!'), nl
-    ; write('Sala não encontrada!'), nl
+        print_sucesso('Capacidade atualizada com sucesso!'), nl
+    ; print_erro('Sala não encontrada!'), nl
     ),
-    nl, submenu_sala.
+    pause, submenu_sala.
 
 processar_submenu_sala("3") :-
     write('Voltando ao menu anterior...'), nl.
 
 processar_submenu_sala(_) :-
-    write('Opção inválida!'), nl,
+    print_erro('Opção inválida!'), pause,
     submenu_sala.
 
 submenu_turma :-
+    draw_header("MENU TURMA"),
     write('1. Editar Horarios'), nl,
     write('2. Editar requisitos'), nl,
     write('3. Realocar novo professor'), nl,
@@ -92,7 +94,6 @@ processar_submenu_turma("2") :- write('Editando requisitos...'),
     nl, submenu_turma.
 processar_submenu_turma("3") :- 
     write('ID da turma: '), read_line_to_string(user_input, ID),
-    listar_professores,
     write('ID do novo professor: ') , read_line_to_string(user_input, Prof),
     realoca_prof(ID, Prof), 
     submenu_turma.
@@ -105,4 +106,44 @@ processar_submenu_turma("5") :-
     write('ID da turma: '), read_line_to_string(user_input, ID),
     remove_class(ID), nl, submenu_turma.
 processar_submenu_turma("6") :- write('Voltando ao menu anterior...'), nl, admin_menu.
-processar_submenu_turma(_) :- write('Opcao invalida!'), nl, submenu_turma.
+processar_submenu_turma(_) :- print_erro('Opcao invalida!'), pause, submenu_turma.
+
+entry_class :-
+    draw_header("CADASTRO DE TURMA"),
+    read_classId(ID),
+    read_disciplina(id,Disciplina),
+    read_curso(id,Curso),
+    listar_professores,
+    read_professor_id(ProfessorID),
+    read_schedule(ID, _, _),
+    read_capacity(Capacidade),
+    read_recursos([],Requisitos),
+    assertz(class(ID, Disciplina, Curso, ProfessorID, Capacidade, Requisitos)),
+    save_classes('rules/classes.pl'),
+    admin_menu.
+
+entry_classroom :-
+    draw_header("CADASTRO DE SALA"),
+    consult('rules/classrooms.pl'),
+    read_classroomId(ID),
+    write('Bloco: '), read_line_to_string(user_input, Bloco),
+    read_capacity(Capacidade),
+    read_recursos([],Recursos),
+    assertz(classroom(ID, Bloco, Capacidade, Recursos)),
+    save_classrooms('rules/classrooms.pl'),
+    admin_menu.
+
+edit_classroom_capacity :-
+    consult('rules/classrooms.pl'),
+    get_classroom(ID),
+    read_capacity(Capacidade),
+    update_capacity(ID,Capacidade).
+
+edit_classroom_resources :-
+    consult('rules/classrooms.pl'),
+    get_classroom(ID),
+    read_recursos([],Recursos),
+    update_resources_classroom(ID,Recursos).
+
+alterar_horario_turma(ID) :-
+    edit_schedule(ID).
